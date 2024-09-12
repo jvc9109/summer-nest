36f2df25c1da9023cfe0da7e2521b968fb6ec8f5
Jorge Vergara <jvc.9109@hotmail.com>
2024-09-12 19:32:12 +0200

    project 6

--- main.js
+++ main.js
@@ -1,140 +1,98 @@
-import RebillyAPI from "rebilly-js-sdk";
+const bookingAmount = 2000;
 
-RebillyInstruments.mount({
-    publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
-    organizationId: 'summer-nest---phronesis',
-    websiteId: 'rebilly.com',
-    apiMode: 'sandbox',
-    items: [
-        {
-            planId: 'monthly',
-            quantity: 1,
-            thumbnail: 'https://api-sandbox.rebilly.com/files/file_01J4F2DA4KFCYRRWRWPKTWTRGT/permalink/c9cc5246-8a18-49e8-b552-c836e8e61803',
+
+//when click button with id 20p-deposit, update RebillyInstruments
+document.getElementById('20p-deposit').addEventListener('click', function() {
+    RebillyInstruments.destroy();
+    RebillyInstruments.mount({
+        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+        organizationId: 'summer-nest---phronesis',
+        websiteId: 'rebilly.com',
+        apiMode: 'sandbox',
+        money: {
+            amount: bookingAmount*0.2,
+            currency: 'USD'
         },
-    ],
-    paymentInstruments: {
-        address: {
-            show: ['email', 'phoneNumber', 'country', 'city'],
+
+        paymentInstruments: {
+            address: {
+                show: ['email', 'phoneNumber', 'country', 'city'],
+            }
+        },
+        theme: {
+            colorPrimary: '#3D3839',
         }
-    },
-    theme: {
-        colorPrimary: '#3D3839',
-    },
-    addons: [
-        {
-            planId: 'personalized-travel-plan',
-            quantity: 1,
+    });
+});
+
+document.getElementById('30p-deposit').addEventListener('click', function() {
+    RebillyInstruments.destroy();
+    RebillyInstruments.mount({
+        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+        organizationId: 'summer-nest---phronesis',
+        websiteId: 'rebilly.com',
+        apiMode: 'sandbox',
+        money: {
+            amount: bookingAmount*0.3,
+            currency: 'USD'
         },
-    ],
-    bumpOffer: [
-        {
-            planId: 'monthly-platinum',
-            quantity: 1,
+
+        paymentInstruments: {
+            address: {
+                show: ['email', 'phoneNumber', 'country', 'city'],
+            }
         },
-    ],
-    features: {
-        hideContinue: true,
-    }
-}).then(() => {
-    console.log('Instruments mounted');
+        theme: {
+            colorPrimary: '#3D3839',
+        }
+    });
 });
 
-// Optional
-RebillyInstruments.on('instrument-ready', (instrument) => {
-    console.info('instrument-ready', instrument);
-});
-RebillyInstruments.on('purchase-completed', (purchase) => {
-    console.info('purchase-completed', purchase);
-});
+document.getElementById('50p-deposit').addEventListener('click', function() {
+    RebillyInstruments.destroy();
+    RebillyInstruments.mount({
+        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+        organizationId: 'summer-nest---phronesis',
+        websiteId: 'rebilly.com',
+        apiMode: 'sandbox',
+        money: {
+            amount: bookingAmount*0.5,
+            currency: 'USD'
+        },
 
-const appState = {
-    isMonthly: true,
-    plan: ['yearly', 'monthly'],
-    bumpOfferPlan: ['yearly-platinum', 'monthly-platinum']
-};
-
-async function updatePlan(e) {
-    e.preventDefault();
-    e.target.disabled = true;
-
-    appState.isMonthly = !appState.isMonthly;
-
-    //const updatedLocale = appState.localeChanged ? 'es' : 'en';
-    updatePlanButton.textContent = appState.isMonthly ? 'Switch to yearly plan' : 'Switch to monthly plan';
-    const newConfig = {
-        items: [
-            {
-                planId: appState.plan[+appState.isMonthly],
-                quantity: 1,
-                thumbnail: 'https://api-sandbox.rebilly.com/files/file_01J4F2DA4KFCYRRWRWPKTWTRGT/permalink/c9cc5246-8a18-49e8-b552-c836e8e61803',
-            },
-        ],
-        bumpOffer: [
-            {
-                planId: appState.bumpOfferPlan[+appState.isMonthly],
-                quantity: 1,
-            },
-        ],
-    };
-    try {
-        await RebillyInstruments.update(newConfig);
-    } catch (error) {
-        console.log('Error updating instruments: ', error);
-    } finally {
-        e.target.disabled = false;
-    }
-}
-
-const updatePlanButton = document.getElementById('update-plan');
-updatePlanButton.addEventListener('click', updatePlan);
-
-
-const termsAndConditions = document.getElementById('terms-and-conditions');
-termsAndConditions.addEventListener('change', () => {
-    const newConfig = {
-        features: {hideContinue: !termsAndConditions.checked}
-    };
-    RebillyInstruments.update(newConfig);
+        paymentInstruments: {
+            address: {
+                show: ['email', 'phoneNumber', 'country', 'city'],
+            }
+        },
+        theme: {
+            colorPrimary: '#3D3839',
+        }
+    });
 });
 
-const handleDonation = async (e) => {
-    // get /api/hello
-    console.log("handling donation");
-    e.preventDefault();
-
-    const form = document.getElementById('payment-form');
-    const formData = new FormData(form);
-
-    const amount = formData.get('amount');
-    const paymentType = formData.get('payment-type');
-
-    const body = {
-        amount: parseFloat(amount),
-        paymentType: paymentType,
-    };
-
-    const response = await fetch('/api/makedonation',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
-
-    const data = await response.json();
-    const newConfig = {
-        items: [],
-        invoiceId: data.invoiceId,
-        jwt: data.jwt,
-    };
-
-    RebillyInstruments.update(newConfig);
-
-    document.getElementById('payment-form').style.display = 'none';
-
-}
-
-document.getElementById('payment-form').onsubmit = handleDonation;
+document.getElementById('custom-deposit').addEventListener('click', function() {
+    RebillyInstruments.destroy();
+    const customAmount = parseFloat(document.getElementById('deposit-amount').value);
 
 
+    RebillyInstruments.mount({
+        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+        organizationId: 'summer-nest---phronesis',
+        websiteId: 'rebilly.com',
+        apiMode: 'sandbox',
+        money: {
+            amount: Math.max(bookingAmount*0.2, Math.min(customAmount, bookingAmount )),
+            currency: 'USD'
+        },
+
+        paymentInstruments: {
+            address: {
+                show: ['email', 'phoneNumber', 'country', 'city'],
+            }
+        },
+        theme: {
+            colorPrimary: '#3D3839',
+        }
+    });
+});
---
+++ project-5/main.js
@@ -0,0 +1,139 @@
+import RebillyAPI from "rebilly-js-sdk";
+
+RebillyInstruments.mount({
+    publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+    organizationId: 'summer-nest---phronesis',
+    websiteId: 'rebilly.com',
+    apiMode: 'sandbox',
+    items: [
+        {
+            planId: 'monthly',
+            quantity: 1,
+            thumbnail: 'https://api-sandbox.rebilly.com/files/file_01J4F2DA4KFCYRRWRWPKTWTRGT/permalink/c9cc5246-8a18-49e8-b552-c836e8e61803',
+        },
+    ],
+    paymentInstruments: {
+        address: {
+            show: ['email', 'phoneNumber', 'country', 'city'],
+        }
+    },
+    theme: {
+        colorPrimary: '#3D3839',
+    },
+    addons: [
+        {
+            planId: 'personalized-travel-plan',
+            quantity: 1,
+        },
+    ],
+    bumpOffer: [
+        {
+            planId: 'monthly-platinum',
+            quantity: 1,
+        },
+    ],
+    features: {
+        hideContinue: true,
+    }
+}).then(() => {
+});
+
+// Optional
+RebillyInstruments.on('instrument-ready', (instrument) => {
+    console.info('instrument-ready', instrument);
+});
+RebillyInstruments.on('purchase-completed', (purchase) => {
+    console.info('purchase-completed', purchase);
+});
+
+const appState = {
+    isMonthly: true,
+    plan: ['yearly', 'monthly'],
+    bumpOfferPlan: ['yearly-platinum', 'monthly-platinum']
+};
+
+async function updatePlan(e) {
+    e.preventDefault();
+    e.target.disabled = true;
+
+    appState.isMonthly = !appState.isMonthly;
+
+    //const updatedLocale = appState.localeChanged ? 'es' : 'en';
+    updatePlanButton.textContent = appState.isMonthly ? 'Switch to yearly plan' : 'Switch to monthly plan';
+    const newConfig = {
+        items: [
+            {
+                planId: appState.plan[+appState.isMonthly],
+                quantity: 1,
+                thumbnail: 'https://api-sandbox.rebilly.com/files/file_01J4F2DA4KFCYRRWRWPKTWTRGT/permalink/c9cc5246-8a18-49e8-b552-c836e8e61803',
+            },
+        ],
+        bumpOffer: [
+            {
+                planId: appState.bumpOfferPlan[+appState.isMonthly],
+                quantity: 1,
+            },
+        ],
+    };
+    try {
+        await RebillyInstruments.update(newConfig);
+    } catch (error) {
+        console.log('Error updating instruments: ', error);
+    } finally {
+        e.target.disabled = false;
+    }
+}
+
+const updatePlanButton = document.getElementById('update-plan');
+updatePlanButton.addEventListener('click', updatePlan);
+
+
+const termsAndConditions = document.getElementById('terms-and-conditions');
+termsAndConditions.addEventListener('change', () => {
+    const newConfig = {
+        features: {hideContinue: !termsAndConditions.checked}
+    };
+    RebillyInstruments.update(newConfig);
+});
+
+const handleDonation = async (e) => {
+    // get /api/hello
+    console.log("handling donation");
+    e.preventDefault();
+
+    const form = document.getElementById('payment-form');
+    const formData = new FormData(form);
+
+    const amount = formData.get('amount');
+    const paymentType = formData.get('payment-type');
+
+    const body = {
+        amount: parseFloat(amount),
+        paymentType: paymentType,
+    };
+
+    const response = await fetch('/api/makedonation',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+
+    const data = await response.json();
+    const newConfig = {
+        items: [],
+        invoiceId: data.invoiceId,
+        jwt: data.jwt,
+    };
+
+    RebillyInstruments.update(newConfig);
+
+    document.getElementById('payment-form').style.display = 'none';
+
+}
+
+document.getElementById('payment-form').onsubmit = handleDonation;
+
+
