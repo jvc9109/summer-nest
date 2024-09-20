b64162e6d5f19dfc856d590fe165e03f0942f33a
Jorge Vergara <jvc.9109@hotmail.com>
2024-09-20 16:06:44 +0200

    project 7

--- main.js
+++ main.js
@@ -1,98 +1,118 @@
 const bookingAmount = 2000;
 
+function project6 () {
+    //when click button with id 20p-deposit, update RebillyInstruments
+    document.getElementById('20p-deposit').addEventListener('click', function() {
+        RebillyInstruments.destroy();
+        RebillyInstruments.mount({
+            publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+            organizationId: 'summer-nest---phronesis',
+            websiteId: 'rebilly.com',
+            apiMode: 'sandbox',
+            money: {
+                amount: bookingAmount*0.2,
+                currency: 'USD'
+            },
 
-//when click button with id 20p-deposit, update RebillyInstruments
-document.getElementById('20p-deposit').addEventListener('click', function() {
-    RebillyInstruments.destroy();
-    RebillyInstruments.mount({
-        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
-        organizationId: 'summer-nest---phronesis',
-        websiteId: 'rebilly.com',
-        apiMode: 'sandbox',
-        money: {
-            amount: bookingAmount*0.2,
-            currency: 'USD'
-        },
-
-        paymentInstruments: {
-            address: {
-                show: ['email', 'phoneNumber', 'country', 'city'],
+            paymentInstruments: {
+                address: {
+                    show: ['email', 'phoneNumber', 'country', 'city'],
+                }
+            },
+            theme: {
+                colorPrimary: '#3D3839',
             }
-        },
-        theme: {
-            colorPrimary: '#3D3839',
-        }
+        });
+    });
+
+    document.getElementById('30p-deposit').addEventListener('click', function() {
+        RebillyInstruments.destroy();
+        RebillyInstruments.mount({
+            publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+            organizationId: 'summer-nest---phronesis',
+            websiteId: 'rebilly.com',
+            apiMode: 'sandbox',
+            money: {
+                amount: bookingAmount*0.3,
+                currency: 'USD'
+            },
+
+            paymentInstruments: {
+                address: {
+                    show: ['email', 'phoneNumber', 'country', 'city'],
+                }
+            },
+            theme: {
+                colorPrimary: '#3D3839',
+            }
+        });
+    });
+
+    document.getElementById('50p-deposit').addEventListener('click', function() {
+        RebillyInstruments.destroy();
+        RebillyInstruments.mount({
+            publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+            organizationId: 'summer-nest---phronesis',
+            websiteId: 'rebilly.com',
+            apiMode: 'sandbox',
+            money: {
+                amount: bookingAmount*0.5,
+                currency: 'USD'
+            },
+
+            paymentInstruments: {
+                address: {
+                    show: ['email', 'phoneNumber', 'country', 'city'],
+                }
+            },
+            theme: {
+                colorPrimary: '#3D3839',
+            }
+        });
+    });
+
+    document.getElementById('custom-deposit').addEventListener('click', function() {
+        RebillyInstruments.destroy();
+        const customAmount = parseFloat(document.getElementById('deposit-amount').value);
+
+
+        RebillyInstruments.mount({
+            publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
+            organizationId: 'summer-nest---phronesis',
+            websiteId: 'rebilly.com',
+            apiMode: 'sandbox',
+            money: {
+                amount: Math.max(bookingAmount*0.2, Math.min(customAmount, bookingAmount )),
+                currency: 'USD'
+            },
+
+            paymentInstruments: {
+                address: {
+                    show: ['email', 'phoneNumber', 'country', 'city'],
+                }
+            },
+            theme: {
+                colorPrimary: '#3D3839',
+            }
+        });
+    });
+}
+
+//take this json from /api/getinvoices make a table in html with ID, Due Time, Issued Time, Amount, Status, Payment Form URL, created time
+
+fetch('/api/getinvoices')
+.then(response => response.json())
+.then(data => {
+    const table = document.getElementById('invoice-table');
+    data.invoices.forEach(invoice => {
+        const row = table.insertRow();
+        row.insertCell(0).textContent = invoice.id;
+        row.insertCell(1).textContent = invoice.dueTime;
+        row.insertCell(2).textContent = invoice.issuedTime;
+        row.insertCell(3).textContent = `${invoice.amount} $`;
+        row.insertCell(4).textContent = invoice.status;
+        row.insertCell(5).innerHTML = invoice.paymentFormUrl ? `<a href=${invoice.paymentFormUrl}> Pay Now </a>` : 'Paid';
+        row.insertCell(6).textContent = invoice.createdTime;
     });
 });
 
-document.getElementById('30p-deposit').addEventListener('click', function() {
-    RebillyInstruments.destroy();
-    RebillyInstruments.mount({
-        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
-        organizationId: 'summer-nest---phronesis',
-        websiteId: 'rebilly.com',
-        apiMode: 'sandbox',
-        money: {
-            amount: bookingAmount*0.3,
-            currency: 'USD'
-        },
-
-        paymentInstruments: {
-            address: {
-                show: ['email', 'phoneNumber', 'country', 'city'],
-            }
-        },
-        theme: {
-            colorPrimary: '#3D3839',
-        }
-    });
-});
-
-document.getElementById('50p-deposit').addEventListener('click', function() {
-    RebillyInstruments.destroy();
-    RebillyInstruments.mount({
-        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
-        organizationId: 'summer-nest---phronesis',
-        websiteId: 'rebilly.com',
-        apiMode: 'sandbox',
-        money: {
-            amount: bookingAmount*0.5,
-            currency: 'USD'
-        },
-
-        paymentInstruments: {
-            address: {
-                show: ['email', 'phoneNumber', 'country', 'city'],
-            }
-        },
-        theme: {
-            colorPrimary: '#3D3839',
-        }
-    });
-});
-
-document.getElementById('custom-deposit').addEventListener('click', function() {
-    RebillyInstruments.destroy();
-    const customAmount = parseFloat(document.getElementById('deposit-amount').value);
-
-
-    RebillyInstruments.mount({
-        publishableKey: 'pk_sandbox_xYh76FWucrGzClSBRDMHxUR9HsuuKM0xm_9I00V',
-        organizationId: 'summer-nest---phronesis',
-        websiteId: 'rebilly.com',
-        apiMode: 'sandbox',
-        money: {
-            amount: Math.max(bookingAmount*0.2, Math.min(customAmount, bookingAmount )),
-            currency: 'USD'
-        },
-
-        paymentInstruments: {
-            address: {
-                show: ['email', 'phoneNumber', 'country', 'city'],
-            }
-        },
-        theme: {
-            colorPrimary: '#3D3839',
-        }
-    });
-});
--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,6 @@
-import express, { Router } from "express";
+import express, {Router} from "express";
 import serverless from "serverless-http";
 import RebillyAPI from "rebilly-js-sdk";
-import bodyParser from "body-parser";
 
 export async function handler(event, context) {
     const app = express();
@@ -20,7 +19,7 @@ export async function handler(event, context) {
 
     router.post('/makedonation', async (req, res) => {
         // req.body is a binary, how to get the body params
-        const { amount, paymentType } = req.body;
+        const {amount, paymentType} = req.body;
 
         console.log(req.body);
 
@@ -29,10 +28,10 @@ export async function handler(event, context) {
             mode: 'passwordless',
             customerId: customer.fields.id,
         };
-        const { fields: login } = await api.customerAuthentication.login({
+        const {fields: login} = await api.customerAuthentication.login({
             data,
         });
-        const { fields: exchangeToken } =
+        const {fields: exchangeToken} =
             await api.customerAuthentication.exchangeToken({
                 token: login.token,
                 data: {
@@ -66,40 +65,40 @@ export async function handler(event, context) {
             websiteId: 'rebilly.com',
         }
         if (paymentType === 'one-time') {
-            subsData.items =  [
-                    {
-                        quantity: 1,
-                        plan: {
-                            id: 'donation-one-time',
-                            name: 'flexible-donation',
-                            productId: 'donation',
-                            currency: 'USD',
-                            pricing: {
-                                formula: 'fixed-fee',
-                                price: amount
-                            }
+            subsData.items = [
+                {
+                    quantity: 1,
+                    plan: {
+                        id: 'donation-one-time',
+                        name: 'flexible-donation',
+                        productId: 'donation',
+                        currency: 'USD',
+                        pricing: {
+                            formula: 'fixed-fee',
+                            price: amount
                         }
                     }
-                ];
+                }
+            ];
         } else {
             subsData.items = [
-                    {
-                        plan: {
-                            id: 'donation-monthly',
-                            name: 'flexible-donation',
-                            productId: 'donation',
-                            currency: 'USD',
-                            pricing: {
-                                formula: 'fixed-fee',
-                                price: amount
-                            },
-                            recurringInterval: {
-                                unit: 'month',
-                                length: 1
-                            }
+                {
+                    plan: {
+                        id: 'donation-monthly',
+                        name: 'flexible-donation',
+                        productId: 'donation',
+                        currency: 'USD',
+                        pricing: {
+                            formula: 'fixed-fee',
+                            price: amount
+                        },
+                        recurringInterval: {
+                            unit: 'month',
+                            length: 1
                         }
                     }
-                ];
+                }
+            ];
         }
         try {
             const subscription = await api.subscriptions.create({data: subsData});
@@ -112,6 +111,55 @@ export async function handler(event, context) {
         }
 
 
+    });
+
+    router.get('/getinvoices', async (req, res) => {
+        const invoices = await api.invoices.getAll({
+            filter: 'customerId:cus_01J6HNWJ61NBF3JGA8XH76SVE2',
+            sort: 'createdTime:desc'
+        });
+
+        const data = {
+            mode: 'passwordless',
+            customerId: 'cus_01J6HNWJ61NBF3JGA8XH76SVE2',
+        };
+        const {fields: login} = await api.customerAuthentication.login({
+            data,
+        });
+        const {fields: exchangeToken} =
+            await api.customerAuthentication.exchangeToken({
+                token: login.token,
+                data: {
+                    acl: [
+                        {
+                            scope: {
+                                organizationId: ['summer-nest---phronesis'],
+                            },
+                            permissions: [
+                                'StorefrontGetInvoice',
+                                'StorefrontGetAccount',
+                                'StorefrontGetPlanCollection',
+                                'StorefrontPostPreviewPurchase',
+                                "StorefrontPostPaymentInstrument",
+                                "StorefrontGetPaymentInstrument",
+                                "StorefrontGetPaymentInstrumentCollection",
+                                "StorefrontPostReadyToPay",
+                                "StorefrontGetPaymentInstrumentSetup",
+                                "StorefrontPostPaymentInstrumentSetup",
+                            ],
+                        },
+                    ],
+                    customClaims: {
+                        websiteId: 'rebilly.com',
+                    },
+                },
+            });
+
+        res.json({
+            invoices: invoices.items.map((invoice) => {
+                return invoice.fields;
+            }),
+        });
     });
 
     app.use('/api/', router);
